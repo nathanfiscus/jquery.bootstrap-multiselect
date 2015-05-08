0893ed7a10c7d76e2758e386bd0ae51d5302e280
nathanfiscus <nathan_fiscus@hotmail.com>
2015-05-07 18:56:22 -0600

    Update to Test file and added live search, button text, and action buttons

--- js/jquery.bootstrap-multiselect.js
+++ js/jquery.bootstrap-multiselect.js
@@ -7,6 +7,13 @@
 
 'use strict';
 
+//Case Insensitive Contains Selector
+jQuery.expr[":"].icontains = jQuery.expr.createPseudo(function (arg) {
+    return function (elem) {
+        return jQuery(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
+
 (function ($) {
     var defaultOptions = {
         width:"auto",
@@ -16,7 +23,7 @@
         searchInsensitive: true,
         liveSearch: true,
         countSelectedText: function (numSelected, numTotal) {
-            return (numSelected == 1) ? "{0} item selected" : "{0} items selected";
+            return (numSelected == 1) ? "{0} item selected" : "{0} of {1} items selected";
         },
         autoHeight: true,
         keepSearchFocus: false,
@@ -31,6 +38,25 @@
         destroy: function(){}
     };
 
+    var  htmlEscape = function(html) {
+        var escapeMap = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#x27;',
+            '`': '&#x60;'
+        };
+        var source = '(?:' + Object.keys(escapeMap).join('|') + ')',
+            testRegexp = new RegExp(source),
+            replaceRegexp = new RegExp(source, 'g'),
+            string = html == null ? '' : '' + html;
+        return testRegexp.test(string) ? string.replace(replaceRegexp, function (match) {
+            return escapeMap[match];
+        }) : string;
+    };
+
+    //jQuery Plugin Definition
     $.fn.multiselect = function (methodOrOptions) {
         if (methods[methodOrOptions]) {
             return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -42,19 +68,42 @@
         }
     };
 
+    //Global Items array
     $.fn.multiselect.items = [];
 
+    //Object Initialization
     methods.init = function (_options) {
         var options = $.extend({}, defaultOptions, _options);
         return this.each(function () {
             var $me = $(this);
+            var title = "";
+            var totalItemCount = 0;
+            var totalSelectedItemCount = 0;
+            var searchbox = options.liveSearch ? '<div class="bms-searchbox" style=""><input type="text" class="input-block-level form-control" autocomplete="off" style="margin-bottom: 0px;"/></div>' : '';
+            var actionsbox = options.actionsBox ? '<div class="bms-actionsbox">' +
+                                                    '<div class="btn-group btn-block">' +
+                                                    '<button class="actions-btn bms-select-all btn btn-sm btn-default">' +
+                                                    'Select All' +
+                                                    '</button>' +
+                                                    '<button class="actions-btn bms-deselect-all btn btn-sm btn-default">' +
+                                                    'Unselect All' +
+                                                    '</button>' +
+                                                    '</div>' +
+                                                    '</div>' : '';
+            var btn = '<button class="compatibilitySelectButton btn-multiselect btn-default dropdown-toggle btn" data-toggle="dropdown"  data-multiselect="'+ $me.attr('id') + '" data-status="closed" data-toggle="dropdown">';
+            if (options.showCaret)
+                btn += '<span class="caret"></span>';
+            btn += '</button>';
+
             $me.addClass("dropdown-menu")
                 .attr("role","menu")
+                .prepend(actionsbox)
+                .prepend(searchbox)
                 .wrap('<div style="width:100%;"></div>').parent()
-                .prepend('<button class="compatibilitySelectButton btn-multiselect btn-default dropdown-toggle btn" data-multiselect="'+ $me.attr('id') + '" data-status="closed" data-toggle="dropdown" style="font-size: 11px; width: 100%;">&nbsp;</button>')
+                .prepend(btn)
                 .addClass("btn-group")
                 .addClass("multiselect-container");
-            //
+
             var $btn = $('.btn-multiselect[data-multiselect="'+ $me.attr('id') + '"]');
             var $container = $btn.parent();
 
@@ -64,10 +113,38 @@
             //Create Item Array
             var arr = [];
             $me.data("multiselect-id",$.fn.multiselect.items.length);
-            $me.children().each(function(){
-                arr.push($(this));
+            $me.children('li').each(function(index){
+                var $t = $(this);
+                var $chk = $('input[type="checkbox"]',$t);
+                var obj = {text: $t.text(),value: $chk.attr('value'), checked: $chk.is(':checked')};
+                $t.data('ms-index',index);
+                arr.push(obj);
+                if($chk.is(':checked')){
+                    totalSelectedItemCount++;
+                }
+                totalItemCount++;
             });
             $.fn.multiselect.items.push(arr);
+
+            if (totalItemCount == 0) {
+                $button.css("min-height", "32px");
+                $button.attr('disabled', true);
+                title = "No Options Available";
+                if (options.showCaret)
+                    title += '<span class="caret"></span>';
+                $button.html(title);
+            }
+
+            if (totalSelectedItemCount > 0) {
+                var tr8nText = (typeof options.countSelectedText === 'function') ? options.countSelectedText(totalSelectedItemCount, totalItemCount) : options.countSelectedText;
+                title = tr8nText.replace('{0}', totalSelectedItemCount.toString()).replace('{1}', totalItemCount.toString());
+            } else {
+                title = options.noneSelectedText;
+            }
+            //title = htmlEscape(title);
+            if (options.showCaret)
+                title += '<span class="caret"></span>';
+            $btn.html(title);
 
             $btn.on("click", function(e){
                 $btn.clicked = true;
@@ -92,8 +169,35 @@
             $('li',$me).on("click",function(e){
                 e.preventDefault();
                 e.stopPropagation();
-                $('input[type="checkbox"]',$(this)).trigger("click");
+                var $t = $(this);
+                var $chk = $('input[type="checkbox"]',$t);
+                var dropID = parseInt($me.data("multiselect-id"));
+                var itemIndex = parseInt($t.data("ms-index"));
+                $.fn.multiselect.items[dropID][itemIndex].checked = !$.fn.multiselect.items[dropID][itemIndex].checked;
+                if($.fn.multiselect.items[dropID][itemIndex].checked){
+                    totalSelectedItemCount++;
+                }else{
+                    totalSelectedItemCount--;
+                }
+                $chk.trigger("click");
+
+                if (options.liveSearch && options.keepSearchFocus){
+                    $('input[type="text"]', $me).focus();
+                }
+                //Update Title
+                if (totalSelectedItemCount > 0) {
+                    var tr8nText = (typeof options.countSelectedText === 'function') ? options.countSelectedText(totalSelectedItemCount, totalItemCount) : options.countSelectedText;
+                    title = tr8nText.replace('{0}', totalSelectedItemCount.toString()).replace('{1}', totalItemCount.toString());
+                } else {
+                    title = options.noneSelectedText;
+                }
+                //title = htmlEscape(title);
+                if (options.showCaret) {
+                    title += '<span class="caret"></span>';
+                }
+                $btn.html(title);
             });
+
 
             if(!$.fn.multiselect._closers){
                 $.fn.multiselect._closers = [];
@@ -132,6 +236,137 @@
 
             //Make Sure HTML event is only applied once
             $("html").off("click",multiselectCloser).on("click",multiselectCloser);
+
+            //Action Buttons (Select All \ UnSelect All
+            if (options.actionsBox) {
+                //Select All Button
+                $('.bms-select-all', $me).on('click', function () {
+                    var newunselected;
+                    if(totalItemCount == totalSelectedItemCount){
+                        if (options.liveSearch && options.keepSearchFocus){
+                            $('input[type="text"]', $me).focus();
+                        }
+                        return;
+                    }
+                    newunselected = $me.children('li:not(.ms-hide)').filter(function(){ return $('input[type="checkbox"]:checked',this).size() == 0}).each(function(index) {
+                        var $t = $(this);
+                        var $chk = $('input[type="checkbox"]',$t);
+                        var dropID = parseInt($me.data("multiselect-id"));
+                        var itemIndex = parseInt($t.data("ms-index"));
+                        $.fn.multiselect.items[dropID][itemIndex].checked = true;
+                        $chk.trigger('click');
+                        totalSelectedItemCount ++;
+                    });
+
+                    if (newunselected.length > 0) {
+                        $me.trigger("multiselect.itemclicked", {items: newunselected});
+                    }
+
+                    if (options.liveSearch && options.keepSearchFocus){
+                        $('input[type="text"]', $me).focus();
+                    }
+                    //Update Title
+                    if (totalSelectedItemCount > 0) {
+                        var tr8nText = (typeof options.countSelectedText === 'function') ? options.countSelectedText(totalSelectedItemCount, totalItemCount) : options.countSelectedText;
+                        title = tr8nText.replace('{0}', totalSelectedItemCount.toString()).replace('{1}', totalItemCount.toString());
+                    } else {
+                        title = options.noneSelectedText;
+                    }
+                    //title = htmlEscape(title);
+                    if (options.showCaret) {
+                        title += '<span class="caret"></span>';
+                    }
+                    $btn.html(title);
+                });
+                //Unselect All Button
+                $('.bms-deselect-all', $me).on('click', function () {
+                    var newunselected;
+                    if(0 == totalSelectedItemCount){
+                        if (options.liveSearch && options.keepSearchFocus){
+                            $('input[type="text"]', $me).focus();
+                        }
+                        return;
+                    }
+                    newunselected = $me.children('li:not(.ms-hide)').filter(function(){ return $('input[type="checkbox"]:checked',this).size() == 1}).each(function(index) {
+                        var $t = $(this);
+                        var $chk = $('input[type="checkbox"]',$t);
+                        var dropID = parseInt($me.data("multiselect-id"));
+                        var itemIndex = parseInt($t.data("ms-index"));
+                        $.fn.multiselect.items[dropID][itemIndex].checked = false;
+                        $chk.trigger('click');
+                        totalSelectedItemCount --;
+                    });
+
+                    if (newunselected.length > 0) {
+                        $me.trigger("multiselect.itemclicked", {items: newunselected});
+                    }
+
+                    if (options.liveSearch && options.keepSearchFocus){
+                            $('input[type="text"]', $me).focus();
+                    }
+                    //Update Title
+                    if (totalSelectedItemCount > 0) {
+                        var tr8nText = (typeof options.countSelectedText === 'function') ? options.countSelectedText(totalSelectedItemCount, totalItemCount) : options.countSelectedText;
+                        title = tr8nText.replace('{0}', totalSelectedItemCount.toString()).replace('{1}', totalItemCount.toString());
+                    } else {
+                        title = options.noneSelectedText;
+                    }
+                    //title = htmlEscape(title);
+                    if (options.showCaret) {
+                        title += '<span class="caret"></span>';
+                    }
+                    $btn.html(title);
+                });
+
+            }
+
+            //Live Search
+            if (options.liveSearch) {
+                $me.searchtimer = null;
+                $('input[type="text"]',$me).on('input propertychange', function (item) {
+                    clearTimeout($me.searchtimer);
+                    var ms = 500; // milliseconds
+                    var $this = $(this);
+                    $me.searchtimer = setTimeout(function () {
+                        $me.children().filter(".no-results").remove();
+                        var no_results = $('<li class="no-results"></li>');
+                        if ($this.val()) {
+                            if (options.searchInsensitive) {
+                                $me.children('li').removeClass('ms-hide').not(':icontains(' + $this.val() + ')').addClass('ms-hide');
+                            } else {
+                                $me.children('li').removeClass('ms-hide').not(':contains(' + $this.val() + ')').addClass('ms-hide');
+                            }
+
+                            if (!$me.find('li').filter(':visible:not(.no-results)').length) {
+                                if (!!no_results.parent().length) no_results.remove();
+                                no_results.html(options.noneResultsText + ' "' + $this.val() + '"').show();
+                                $me.find('li').last().after(no_results);
+                            } else if (!!no_results.parent().length) {
+                                no_results.remove();
+                            }
+
+                        } else {
+                            $me.children('li').not('.is-hidden').removeClass('ms-hide');
+                            if (no_results.parent().length) no_results.remove();
+                        }
+
+                        $me.children('li.active').removeClass('active');
+                        $me.children('li').filter(':visible:not(.divider)').eq(0).addClass('active').focus();
+                        $this.focus();
+                    }, ms);
+
+                });
+                $me.on("multiselect.close",function(){
+                    if(options.liveSearch){
+                        $me.children('li.ms-hide').removeClass("ms-hide");
+                        $me.children('li.no-results').remove();
+                        $('input[type="text"]',$me).val("");
+                    }
+                });
+
+            }
+
+
 
         });
     }
